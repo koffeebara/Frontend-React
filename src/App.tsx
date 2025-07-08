6b4a4eba88ad61562ffb740ee62a122fc017c911
ydsic <ydsic99@gmail.com>
2025-07-08 11:02:36 +0900

    Feature/login (#1)
    * feat: login 페이지 및 간단 틀 추가
    
    * fix: 오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
 import Lee from "./pages/Lee";
-import Moon from './pages/Moon';
+import Moon from "./pages/Moon";
+import Login from "./pages/Login";
 
 // npm run format → prettier로 코드 정렬
 // npm run lint → eslint로 코드 검사
 
 function App() {
-
   return (
     <>
-      <Link to="/lee">LEE</Link><br />
-      <Link to="/moon">MOON</Link><hr />
+      {/* 아래 div 코드는 추후 메인페이지가 만들어지면 삭제될 코드 */}
+      <div className="flex flex-col">
+        <Link to="/lee">LEE</Link>
+        <Link to="/moon">MOON</Link>
+        <Link to="/login">LOGIN</Link>
+      </div>
+
       <Routes>
         <Route path="/lee" element={<Lee />} />
         <Route path="/moon" element={<Moon />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
