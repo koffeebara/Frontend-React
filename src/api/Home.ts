2aabb73119f4e93730ca4f0aa890a449c1a7c223
goorm <goorm@goormui-MacBookPro.local>
2025-08-03 02:14:11 +0900

    fix: 배포환경 수정

--- src/api/Home.ts
+++ src/api/Home.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
 export const fetchProducts = async () => {
-  const response = await axios.get("api/products");
+  const response = await axios.get(`${API_BASE_URL}/api/products`);
   return response.data;
 };
