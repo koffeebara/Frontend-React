3f2a268045599804d5f12bd10229a6b84118a5bc
6108 <>
2025-08-02 16:53:50 +0900

    style: 배경색 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@
 export default function NotFound() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-mint-50">
         <h1 className="text-6xl font-bold text-red-600">404</h1>
         <p className="mt-4 text-xl text-gray-700">페이지를 찾을 수 없습니다.</p>
         <p className="mt-2 text-gray-500">요청하신 페이지가 존재하지 않거나, 이동된 경우입니다.</p>
